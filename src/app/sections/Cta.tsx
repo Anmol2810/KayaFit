36bbf9d920c4103f9db0ceea594eb568713fd603
Anmol2810 <ammy.sharma2003@gmail.com>
2025-10-03 19:09:34 +0530

    update

--- src/app/sections/Cta.tsx
+++ src/app/sections/Cta.tsx
@@ -6,7 +6,7 @@ export default function Cta() {
             <div className="w-full max-w animate-border rounded-2xl bg-conic/[from_var(--border-angle)] from-black via-[#ff6361] to-black from-0% via-10% to-20% p-px">
                 <div className="p-10 text-center rounded-2xl bg-black">
                     <div className="flex justify-center md:block">
-                        <Image src="/images/3d_hello.png" alt="hello" width={70} height={70} className="md:ml-100"/>
+                        <Image src="/images/3d_hello.png" alt="hello" width={70} height={70} unoptimized className="md:ml-100"/>
                     </div>
                     <h1 className="mb-5 text-5xl text-white font-black capitalize">Say <span className="gradientText">Hello</span> to your new Self!!</h1>
                     <div className="flex justify-center gap-5"><button className="bg-white text-black p-3 rounded-3xl">Join the Community</button><Link href="/call" className="text-white underline mt-3 font-bold">Book A Call</Link></div>
